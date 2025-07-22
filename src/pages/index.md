bc330e4a1477691ea18be68f6533e6bf0442a600
Jess Crees <jessicacrees@gmail.com>
2025-07-22 19:18:58 +0100

    Implement dynamic layout

--- src/pages/index.md
+++ src/pages/index.md
@@ -1,5 +1,5 @@
 ---
-layout: ../layouts/BaseLayout.astro
+layout: ../layouts/HomePageLayout.astro
 pageTitle: Jess Crees | Digital Garden
 heading: Jess writes about programming & design
 subHeading: Web developer and UX advocate since 2015
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,8 +4,8 @@ import { getCollection } from 'astro:content';
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: 'Jess Crees | Blog',
-    description: 'My digital garden',
+    title: 'Jess Crees | Digital Garden',
+    description: 'Jess writes about programming & design. Web developer and UX advocate since 2015',
     site: context.site,
     items: posts.map((post) => ({
       title: post.data.title,
